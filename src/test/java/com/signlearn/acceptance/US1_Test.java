5291ecface491a91ea5345bc4b401a19cbab1d9f
Faiz <mfaiz6002@gmail.com>
2025-09-24 13:06:54 +1000

    Sol logo and US1

---
+++ src/test/java/com/signlearn/acceptance/US1_Test.java
@@ -0,0 +1,19 @@
+package com.signlearn.acceptance;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class US1_Test
+    {
+        private boolean resourceExistsCheck(String filePath)
+        {
+            return US1_Test.class.getResource(filePath) != null;
+        }
+
+        @Test
+        void logoExistsCheck()
+        {
+            boolean exists = resourceExistsCheck("/images/logo.png");
+            assertTrue(exists, "Logo image exists in project path /images/logo.png");
+        }
+    }
