1671049c8286b22388eb7134ddb68e3882400607
serze1 <serzenjak@gmail.com>
2025-11-08 16:12:44 +1000

    Added validation to Email object

--- src/main/java/com/signlearn/domain/value/Email.java
+++ src/main/java/com/signlearn/domain/value/Email.java
@@ -1,5 +1,23 @@
 package com.signlearn.domain.value;
 
 public record Email(String value) {
-    // TODO: validate email format
+    public Email
+    {
+        if (value == null)
+        {
+            throw new IllegalArgumentException("Emaill can't be null");
+        }
+
+        int atIndex = value.indexOf('@');
+        if (atIndex <= 0 || atIndex == value.length() - 1)
+        {
+            throw new IllegalArgumentException("Email invalid: " + value);
+        }
+
+        String domainName = value.substring(atIndex + 1);
+        if (!domainName.contains("."))
+        {
+            throw new IllegalArgumentException("Email invalid: " + value);
+        }
+    }
 }
